3b605a5bdacaae0f3ebda21ef7ea1de9d32787bd
shrayank123 <74706668+shrayank123@users.noreply.github.com>
2024-08-06 22:18:15 +0700

    Add files via upload

---
+++ dataOfDef.py
@@ -0,0 +1,17 @@
+Jc1Def = [
+    "Kanike", "Darren", "Jinseo", "Woocheol", "Billy",
+    "Daylen", "Noval", "Rama", "YK", "Dennis",
+    "Fatih", "Anan", "Kyusha", "Giselle O", "Giselle F",
+    "Jennifer", "Ola", "Tarra", "Nadia", "Hana",
+    "Charlotte"
+]
+
+Jc1Absent = []
+Jc1Present = []
+
+# JSON paths
+sis_path = 'sis_smg.json'
+Jc1_path = 'jc1.json'
+Jc2_path = 'jc2.json'
+Jc1Txt = 'jc1.txt'
+std_class_path = 'std_class.json'
